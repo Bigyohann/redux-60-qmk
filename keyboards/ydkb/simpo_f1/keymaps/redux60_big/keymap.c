a2a7c692eea20779ff19f900b0aa9538ba41e0e8
Yohann BIGLIA <contact@yohannbiglia.fr>
2025-09-26 15:51:16 +0200

    add few layers

--- keyboards/ydkb/simpo_f1/keymaps/redux60_big/keymap.c
+++ keyboards/ydkb/simpo_f1/keymaps/redux60_big/keymap.c
@@ -3,74 +3,121 @@
 #include "matrix_led.c"
 
 enum layer_names {
-  _BASE,
-  _RAISE,
-  _LOWER
+    _BASE,
+    _CODE,
+    _RAISE,
+    _LOWER,
+};
+enum custom_keycodes {
+    KC_GAMING = SAFE_RANGE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* _BASE Layer: Standard typing
- * ,---------------------------------------------------------------------------------.
- * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |BSPC|
- * |---------------------------------------------------------------------------------|
- * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  \  |
- * |---------------------------------------------------------------------------------|
- * | Caps   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  | Enter   |
- * |---------------------------------------------------------------------------------|
- * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift     |
- * |---------------------------------------------------------------------------------|
- * | LCTL | Win | Alt |          Space          | Raise | Win | Fn  | Ctl |
- * `---------------------------------------------------------------------------------'
- */
-[_BASE] = LAYOUT_60_ansi(
-  KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-  KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
-  KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                          MO(_LOWER), MO(_RAISE), KC_APP,  KC_RCTL
-),
-     
-/* _RAISE Layer: Navigation, F-keys, Symbols
- * ,---------------------------------------------------------------------------------.
- * |  `  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |TRNS|
- * |---------------------------------------------------------------------------------|
- * | TRNS   |TRNS |TRNS |  (  |  )  |TRNS |TRNS | PgUp| Up  | PgDn|TRNS |TRNS |TRNS |TRNS |
- * |---------------------------------------------------------------------------------|
- * | TRNS   | Home| End |  {  |  }  |TRNS |TRNS | Left|Down |Right|TRNS |TRNS | Enter   |
- * |---------------------------------------------------------------------------------|
- * | TRNS      |TRNS |TRNS |  [  |  ]  |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS | Shift     |
- * |---------------------------------------------------------------------------------|
- * | TRNS  |TRNS |TRNS |          TRNS           | TRNS  |TRNS |TRNS |TRNS |
- * `---------------------------------------------------------------------------------'
- */
-[_RAISE] = LAYOUT_60_ansi(
-  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,
-  _______, KC_NO,   KC_NO,   KC_LPRN, KC_RPRN, KC_NO,   KC_NO,   KC_PGUP, KC_UP,   KC_PGDN, KC_NO,   KC_NO,   KC_NO,   _______,
-  _______, KC_HOME, KC_END,  KC_LCBR, KC_RCBR, KC_NO,   KC_NO,   KC_LEFT, KC_DOWN, KC_RGHT, KC_NO,   KC_NO,            _______,
-  _______,          KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______,
-  _______, _______, _______,          _______,                          _______, _______, _______, _______
-),
+    /* _BASE Layer: Standard typing
+    * ,---------------------------------------------------------------------------------.
+    * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |BSPC|
+    * |---------------------------------------------------------------------------------|
+    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  \  |
+    * |---------------------------------------------------------------------------------|
+    * | Caps   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  | Enter   |
+    * |---------------------------------------------------------------------------------|
+    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift     |
+    * |---------------------------------------------------------------------------------|
+    * | LCTL | Win | Alt |          Space          | Raise | Win | Fn  | Ctl |
+    * `---------------------------------------------------------------------------------'
+    */
+    [_BASE] = LAYOUT_60_ansi(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                          MO(_LOWER), MO(_RAISE), KC_APP,  KC_RCTL
+    ),
 
-/* _LOWER Layer: System, Media, RGB Control
- * ,---------------------------------------------------------------------------------.
- * |RESET|TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS
- * |---------------------------------------------------------------------------------|
- * | TRNS   |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS | Mute|VolU |VolD |TRNS |TRNS |TRNS |TRNS |
- * |---------------------------------------------------------------------------------|
- * | TRNS   |TRNS |TRNS |TRNS |TRNS |TRNS | Prev|Play/Pause|Next|TRNS |TRNS | Enter   |
- * |---------------------------------------------------------------------------------|
- * | TRNS      |RGB T|RGB M|RGB H|RGB S|RGB V|TRNS |TRNS |TRNS |TRNS |TRNS | Shift     |
- * |---------------------------------------------------------------------------------|
- * | TRNS  |TRNS |TRNS |          TRNS           | TRNS  |TRNS |TRNS |TRNS |
- * `---------------------------------------------------------------------------------'
- */
-[_LOWER] = LAYOUT_60_ansi(
-  RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
-  _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLU, KC_VOLD, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______,          _______,
-  _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______,          _______,
-  _______, _______, _______,          _______,                          _______, _______, _______, _______
+    /* _CODE Layer: Code layer
+    * ,---------------------------------------------------------------------------------.
+    * | Esc |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  |  0  |  -  |  =  |BSPC|
+    * |---------------------------------------------------------------------------------|
+    * | Tab    |  Q  |  W  |  E  |  R  |  T  |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |  \  |
+    * |---------------------------------------------------------------------------------|
+    * | RAISE/Esc   |  A  |  S  |  D  |  F  |  G  |  H  |  J  |  K  |  L  |  ;  |  '  | Enter   |
+    * |---------------------------------------------------------------------------------|
+    * | Shift     |  Z  |  X  |  C  |  V  |  B  |  N  |  M  |  ,  |  .  |  /  | Shift     |
+    * |---------------------------------------------------------------------------------|
+    * | LCTL | Win | Alt |          Space          | Raise | Win | Fn  | Ctl |
+    * `---------------------------------------------------------------------------------'
+    */
+    [_CODE] = LAYOUT_60_ansi(
+    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+    LT(_RAISE, KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+    KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
+    KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                          MO(_LOWER), MO(_RAISE), KC_APP,  KC_RCTL
+    ),
 
-)
-};
+    /* _RAISE Layer: Navigation, F-keys, Symbols
+    * ,---------------------------------------------------------------------------------.
+    * |  `  | F1  | F2  | F3  | F4  | F5  | F6  | F7  | F8  | F9  | F10 | F11 | F12 |TRNS|
+    * |---------------------------------------------------------------------------------|
+    * | TRNS   |TRNS |TRNS |  (  |  )  |TRNS |TRNS | PgUp| Up  | PgDn|TRNS |TRNS |TRNS |TRNS |
+    * |---------------------------------------------------------------------------------|
+    * | TRNS   | Home| End |  {  |  }  |TRNS |TRNS | Left|Down |Right|TRNS |TRNS | Enter   |
+    * |---------------------------------------------------------------------------------|
+    * | TRNS      |TRNS |TRNS |  [  |  ]  |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS | Shift     |
+    * |---------------------------------------------------------------------------------|
+    * | TRNS  |TRNS |TRNS |          TRNS           | TRNS  |TRNS |TRNS |TRNS |
+    * `---------------------------------------------------------------------------------'
+    */
+    [_RAISE] = LAYOUT_60_ansi(
+    KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_GAMING,
+    _______, KC_MS_WH_UP, KC_MS_UP, KC_MS_WH_DOWN, KC_RPRN, KC_NO,   KC_NO,   KC_PGUP, KC_UP,   KC_PGDN, KC_NO,   KC_NO,   KC_NO,   _______,
+    _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, KC_RCBR, KC_NO,   KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, KC_NO,   KC_NO,            _______,
+    _______,          KC_NO,   KC_NO,   KC_LBRC, KC_RBRC, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,            _______,
+    _______, _______, _______,          _______,                          _______, _______, _______, _______
+    ),
 
+    /* _LOWER Layer: System, Media, RGB Control
+    * ,---------------------------------------------------------------------------------.
+    * |RESET|TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS
+    * |---------------------------------------------------------------------------------|
+    * | TRNS   |TRNS |TRNS |TRNS |TRNS |TRNS |TRNS | Mute|VolU |VolD |TRNS |TRNS |TRNS |TRNS |
+    * |---------------------------------------------------------------------------------|
+    * | TRNS   |TRNS |TRNS |TRNS |TRNS |TRNS | Prev|Play/Pause|Next|TRNS |TRNS | Enter   |
+    * |---------------------------------------------------------------------------------|
+    * | TRNS      |RGB T|RGB M|RGB H|RGB S|RGB V|TRNS |TRNS |TRNS |TRNS |TRNS | Shift     |
+    * |---------------------------------------------------------------------------------|
+    * | TRNS  |TRNS |TRNS |          TRNS           | TRNS  |TRNS |TRNS |TRNS |
+    * `---------------------------------------------------------------------------------'
+    */
+    [_LOWER] = LAYOUT_60_ansi(
+    RESET,   _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU,  _______, _______, 
+    _______, _______, _______, _______, _______, _______, _______, KC_MUTE, KC_VOLU, KC_VOLD, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______,          TG(_CODE),
+    _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, _______, _______, _______, _______, _______,          _______,
+    _______, _______, _______,          _______,                          _______, _______, _______, _______
+    )
+};       
+
+// process_record_user function to handle custom keycodes and layer switching
+// store variable for gaming mode
+bool    gaming_mode = false;
+uint8_t mod_state;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    mod_state = get_mods();
+
+    switch (keycode) {
+        case KC_TAB:
+            if (mod_state == MOD_BIT(KC_LALT) && gaming_mode == true) {
+                return false;
+            }
+            return true;
+            break;
+        case KC_GAMING:
+            if (record->event.pressed) {
+                gaming_mode = !gaming_mode;
+            }
+            break;
+    }
+    return true;
+}
