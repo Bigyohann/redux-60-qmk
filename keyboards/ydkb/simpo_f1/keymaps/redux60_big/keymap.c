e891491892f9d51c8170579b97f28a751d1e3e33
Yohann BIGLIA <y.biglia@matawan-mobility.com>
2025-10-08 18:13:06 +0200

    fix gaming

--- keyboards/ydkb/simpo_f1/keymaps/redux60_big/keymap.c
+++ keyboards/ydkb/simpo_f1/keymaps/redux60_big/keymap.c
@@ -10,6 +10,7 @@ enum layer_names {
 };
 enum custom_keycodes {
     KC_GAMING = SAFE_RANGE,
+    GAMING_CAPS
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -29,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT(
     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-    KC_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
+    GAMING_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
     KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,
     KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                          MO(_LOWER), MO(_RAISE), KC_APP,  KC_RCTL
     ),
@@ -105,19 +106,29 @@ uint8_t mod_state;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     mod_state = get_mods();
 
+    if (gaming_mode == true) {
+        // Disable all layers except _BASE when gaming mode is active
+        switch (keycode) {
+            case KC_TAB:
+                if (mod_state == MOD_BIT(KC_LALT)) {
+                    return false;
+                }
+                break;
+            case KC_LGUI:
+                return false;
+                break;
+            case GAMING_CAPS:
+                // simulate control when gaming mode is active
+                if (record->event.pressed) {
+                    register_code(KC_LCTL);
+                }
+                else {
+                    unregister_code(KC_LCTL);
+                }
+                break;
+        }
+    }
     switch (keycode) {
-        case KC_TAB:
-            if (mod_state == MOD_BIT(KC_LALT) && gaming_mode == true) {
-                return false;
-            }
-            return true;
-            break;
-        case KC_LGUI:
-            if (gaming_mode == true) {
-                return false;
-            }
-            return true;
-            break;
         case KC_GAMING:
             if (record->event.pressed) {
                 gaming_mode = !gaming_mode;
@@ -131,6 +142,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 NVIC_SystemReset();
             }
             break;
+        case GAMING_CAPS:
+            if (gaming_mode == false) {
+                // LT to _RAISE layer when gaming mode is inactive and Caps Lock is pressed
+                if (record->event.pressed) {
+                    layer_on(_RAISE);
+                } else {
+                    layer_off(_RAISE);
+                }
+            }
+            break;
+        
+
     }
     return true;
 }
